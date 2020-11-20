3ca1e393e44ff21fca655bace67d34d44f765ce6
AHnoinskyi <ahnoinskyi@luxoft.com>
2020-11-20 18:40:45 +0200

    LineTracing and PhysicsHandle are added to the Pawn object

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -2,6 +2,11 @@
 
 
 #include "Grabber.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -19,7 +24,12 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Grabber is initialized!"));
+	// check for physics handle component
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (!PhysicsHandle)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s has the Grabber component, but no Physics Handle component"), *GetOwner()->GetName());
+	}
 }
 
 
@@ -28,6 +38,54 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get players viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	// Draw a line from player showing the reach
+
+	FVector LineTraceDirection = PlayerViewPointRotation.Vector();
+	FVector LineTraceEnd = PlayerViewPointLocation + LineTraceDirection * Reach;
+
+	DrawDebugLine(
+		GetWorld(), 
+		PlayerViewPointLocation, 
+		LineTraceEnd, 
+		FColor(255, 0, 0), 
+		false, 
+		0.f, 
+		0,
+		5.F
+	);
+
+	// Ray-cast out to certain distance (Reach)
+	FHitResult Hit;
+
+	/**
+	*param2 - Use player collision or visibility collision;
+	*param3 - Ignore owner object in collision
+	*/
+	FCollisionQueryParams TraceParams( FName(TEXT("")), false, GetOwner() );
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+
+	// See wat it hits
+	AActor* ActorHit = Hit.GetActor();
+	
+	// Logging out to test
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Hitted %s"), *ActorHit->GetName());
+	}
 }
 
